0e3bcbc14d4a6dbb8febcf1f92067f2bb3fa076b
j.grimm <j.grimm@seti-media.com>
2024-09-25 15:20:27 -0400

    Fixed bunch of little bugs and added some cool scripts

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -9,15 +9,21 @@ cpu_color="'#$(printf '%02x' $(expr ${cpu_usage%.*} \* 5 / 2))$(printf '%02x' $(
 printf -v cpu_usage "CPU:<span foreground=$cpu_color>%5s%%</span>" "$cpu_usage"
 
 # Get the ram usage
-ram_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+ram_usage=$(free | grep Mem | awk '{printf "%.1f", ($3/$2 * 100.0)}')
 ram_color="'#$(printf '%02x' $(expr ${ram_usage%.*} \* 5 / 2))$(printf '%02x' $(expr 250 - ${ram_usage%.*} \* 5 / 2))00'"
-printf -v ram_usage "RAM:<span foreground=$ram_color> %.1f%%</span>" "$ram_usage"
+# printf -v ram_usage "RAM:<span foreground=$ram_color> %.1f%%</span>" "$ram_usage"
+printf -v ram_usage "RAM:<span foreground=%s> %s%%</span>" "$ram_color" "$ram_usage"
 
 # Get and format date like 2018-10-06 and the time (e.g., 14:01)
 date_formatted=$(date +'%Y-%m-%d %I:%M:%S %p')
 
 # Get the Linux version but remove the "-1-ARCH" part
-linux_version="$(uname -s):<span foreground='#ff33dd'> $(uname -r | cut -d '-' -f1)</span>"
+linux_version="$(uname -s):<span foreground='#ee44ff'> $(uname -r | cut -d '-' -f1)</span>"
+
+keyboard_layout=$(swaymsg -t get_inputs | jq '.[0].xkb_active_layout_name')
+
+keyboard_layout="Keybord:<span foreground='#ee44ff'> ${keyboard_layout//\"}</span>"
+# keyboard_layout="Keybord: $keyboard_layout"
 
 # sound_volume=$(amixer get Master | grep -o '\[[0-9]*%\]' | grep -o '[0-9]*' | sed -n '2p')
 # printf -v sound_volume "Volume : %d%%" "$sound_volume"
@@ -51,5 +57,5 @@ linux_version="$(uname -s):<span foreground='#ff33dd'> $(uname -r | cut -d '-' -
 #
 # echo "$ram_usage | $cpu_usage | $linux_version | $date_formatted <span font='VictorMono Nerd Font medium Italic 10'><span foreground='#ff0000ff'>$linux_version</span>test</span>"
 
-echo "<span font='VictorMono Nerd Font medium Italic 10'>$ram_usage | $cpu_usage | $linux_version | $date_formatted </span>"
+echo "<span font='VictorMono Nerd Font medium Italic 10'>$ram_usage | $cpu_usage | $keyboard_layout | $linux_version | $date_formatted </span>"
 
