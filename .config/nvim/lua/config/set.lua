7ee20d0fe530156e62f0694af7d2e15cf4eba310
FishyG <fishy@fishynetworks.com>
2024-11-11 15:46:15 -0500

    Added options for ufo and disable wrap

--- .config/nvim/lua/config/set.lua
+++ .config/nvim/lua/config/set.lua
@@ -17,15 +17,13 @@ vim.opt.smartindent = true
 
 vim.opt.scrolloff = 5
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 -- vim.opt.
 -- vim.opt.
 -- vim.opt.
 
-vim.cmd.colorscheme("slate")
-
--- Remove the background so it's transparent
-vim.api.nvim_set_hl(0, 'Normal', {ctermbg = nil})
+-- Disable line wrapping by default
+vim.opt.wrap = false
 
 -- Set leader (TODO: learn what it is)
 vim.g.mapleader = " "
@@ -36,6 +34,11 @@ vim.opt.showmode = false
 -- Set the clipboard to the system's clipboard
 --vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("v", "<leader>y", "\"+y")
+
+vim.opt.foldcolumn = '1' -- '0' is not bad
+vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
 
 -- I'm trying to do this :
 --imap <silent> <C-C> <C-R>=string(eval(input("Calculate: ")))<CR>
