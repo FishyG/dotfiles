fcb7a0a612508438c69cf15fe5acb92fa90c4e2c
FishyG <fishy@fishynetworks.com>
2024-11-11 14:28:33 -0500

    Added remap.lua

---
+++ .config/nvim/lua/config/remap.lua
@@ -0,0 +1,8 @@
+
+-- Using ufo provider need remap `zR` and `zM`. 
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+-- Recenter on move down and up
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
